55d84f15c4175f64db584a2aaea55b04cb0a6ed8
gNano <martinianogancedo@hotmail.com>
2024-06-10 13:27:27 -0300

    más cambios, así queda mejor

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./Checkout.css";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
-import { json } from "react-router-dom";
+import CartContext from "../../CartContext/CartContext";
 
 export default function Checkout() {
   const [buyer, setBuyer] = useState({
@@ -10,8 +10,9 @@ export default function Checkout() {
     email: "",
     phone: "",
   });
-
   const [disabled, setDisabled] = useState(false);
+  const [orderID, setOrderID] = useState(null);
+  const { clearCart } = useContext(CartContext);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -35,7 +36,9 @@ export default function Checkout() {
 
       setTimeout(() => {
         localStorage.removeItem("cart");
-        window.location.href = "/";
+        setOrderID(docRef.id);
+        setDisabled(false);
+        clearCart();
       }, 1000);
     } catch (error) {
       console.error("Error adding document: ", error);
@@ -44,48 +47,59 @@ export default function Checkout() {
 
   return (
     <div className="checkoutForm">
-      <h2>Finaliza tu compra</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Nombre"
-          type="text"
-          id="name"
-          name="name"
-          value={buyer.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          placeholder="Apellido"
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={buyer.lastName}
-          onChange={handleChange}
-          required
-        />
-        <input
-          placeholder="Correo electrónico"
-          type="email"
-          id="email"
-          name="email"
-          value={buyer.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          placeholder="Teléfono"
-          type="tel"
-          id="phone"
-          name="phone"
-          value={buyer.phone}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit" disabled={disabled}>
-          Finalizar compra
-        </button>
-      </form>
+      {orderID ? (
+        <div className="orderFinish">
+          <h2>Gracias por su compra!</h2>
+          <p>
+            El ID de su orden es: <strong>{orderID}</strong>
+          </p>
+        </div>
+      ) : (
+        <>
+          <h2>Finaliza tu compra</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Nombre"
+              type="text"
+              id="name"
+              name="name"
+              value={buyer.name}
+              onChange={handleChange}
+              required
+            />
+            <input
+              placeholder="Apellido"
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={buyer.lastName}
+              onChange={handleChange}
+              required
+            />
+            <input
+              placeholder="Correo electrónico"
+              type="email"
+              id="email"
+              name="email"
+              value={buyer.email}
+              onChange={handleChange}
+              required
+            />
+            <input
+              placeholder="Teléfono"
+              type="tel"
+              id="phone"
+              name="phone"
+              value={buyer.phone}
+              onChange={handleChange}
+              required
+            />
+            <button type="submit" disabled={disabled}>
+              Finalizar compra
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
